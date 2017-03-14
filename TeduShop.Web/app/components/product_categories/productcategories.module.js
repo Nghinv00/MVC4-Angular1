7ac973073f871f2b3e31c5911da13a6d21e43444
Hau Le <lethehau90.vn@gmail.com>
2017-03-14 17:46:19 +0700

    add login and implement template

--- TeduShop.Web/app/components/product_categories/productcategories.module.js
+++ TeduShop.Web/app/components/product_categories/productcategories.module.js
@@ -5,23 +5,25 @@
 
     config.$inject = ['$stateProvider', '$urlRouterProvider'];
 
-    function config($stateProvider, $urlRouterProvider)
-    {
+    function config($stateProvider, $urlRouterProvider) {
         $stateProvider
             .state("product_categories", {
-            url: "/product_categories",
-            templateUrl: "/app/components/product_categories/productcategoryListView.html",
-            controller: "productCategoryListController"
-        })
+                url: "/product_categories",
+                parent: 'base',
+                templateUrl: "/app/components/product_categories/productcategoryListView.html",
+                controller: "productCategoryListController"
+            })
             .state("add_product_category", {
-            url: "/add_product_category",
-            templateUrl: "/app/components/product_categories/productcategoryAddView.html",
-            controller: "productCategoryAddController"
+                url: "/add_product_category",
+                parent: 'base',
+                templateUrl: "/app/components/product_categories/productcategoryAddView.html",
+                controller: "productCategoryAddController"
             })
             .state("edit_product_category", {
-            url: "/edit_product_category/:id",
-            templateUrl: "/app/components/product_categories/productcategoryEditView.html",
-            controller: "productCategoryEditController"
-        });
+                url: "/edit_product_category/:id",
+                parent: 'base',
+                templateUrl: "/app/components/product_categories/productcategoryEditView.html",
+                controller: "productCategoryEditController"
+            });
     }
 })();
